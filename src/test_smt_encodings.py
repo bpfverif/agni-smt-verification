e8220ade9967da7bb68562fe577d6d575e472841
bhivam <shivam.kajaria@rutgers.edu>
2024-04-15 15:26:32 +0000

    extracting errors from diff

--- src/test_smt_encodings.py
+++ src/test_smt_encodings.py
@@ -16,6 +16,8 @@ def main():
 
     input_fp = f"./test{insns.replace(',', '-')}{num_insns}/inputs"
     test_dir = input_fp.split("/")[1]
+    ko = f"{test_dir}/kern_outputs"
+    so = f"{test_dir}/smt_outputs"
 
     if not os.path.exists(test_dir):
         os.mkdir(test_dir)
@@ -24,32 +26,47 @@ def main():
     subprocess.run(["./rand", insns, num_insns, input_fp])
 
     # generate outputs
-    kern_outputs_file = open(f"{test_dir}/kern_outputs", "w")
+    kern_outputs_file = open(ko, "w")
     subprocess.run(["./kernel_verifier", input_fp],
             stdout=kern_outputs_file)
     kern_outputs_file.close()
 
-    smt_outputs_file = open(f"{test_dir}/smt_outputs", "w")
+    smt_outputs_file = open(so, "w")
     subprocess.run(["python3","smt_verifier.py", kern_ver, input_fp],
             stdout=smt_outputs_file)
     smt_outputs_file.close()
 
     # do comparison with diff
-    diff_result = subprocess.run([
-            "diff",
-            f"{test_dir}/smt_outputs",
-            f"{test_dir}/kern_outputs"
-        ], stdout=subprocess.PIPE)
+
+    so = "dt1"
+    ko = "dt2"
+
+    diff_result = subprocess.run(
+            f"nl {so} > {so}_nl; rm {so}; nl {ko} > {ko}_nl; rm {ko}; diff {so}_nl {ko}_nl",
+            stdout=subprocess.PIPE, shell=True)
 
     # read diff output
     diff_output = str(diff_result.stdout, encoding='utf-8')
 
     if len(diff_output) == 0:
         print("No difference in output between SMT verifier and kernel verifier.")
-    else:
-        print("Difference in output between SMT verifier and kernel verifier:")
-        print()
-        print(diff_output)
+        return
+
+    diff_lines = diff_output.split("\n")
+    diff_lines = [line for line in diff_lines if len(line) > 0 and line[0].isnumeric()]
+    broken_inputs = [int(num) for line in diff_lines for num in line.split("c")[0].split(",")]
+
+
+    inputs = open(input_fp, "r")
+    input_lines = inputs.readlines()
+    inputs.close()
+
+    broken_inputs = [input_lines[i-1].strip() for i in broken_inputs]
+
+    print("Broken Instructions:")
+    print()
+    print(*broken_inputs, sep="\n")
+
 
 if __name__ == '__main__':
     main()
